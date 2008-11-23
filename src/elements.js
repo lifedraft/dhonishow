291337676ef7a7225a68bbefde38f1e9d643fd37
st <stanislav@lifedraft.de>
2008-11-23 22:14:58 +0100

    elements init class

---
+++ src/elements.js
@@ -0,0 +1,38 @@
+(function(){
+  var elements = DhoniShow.register("elements", function(){
+    
+  });
+  
+  elements.prototype = {
+    
+  };
+
+})();
+
+/*
+
+  <div class="dhonishow">
+    <img src="#" />
+    <img src="#" />
+    <img src="#" />
+  </div>
+  
+  <ol class="dhonishow">
+    <li><img src="#" /></li>
+    <li><img src="#" /></li>
+    <li><img src="#" /></li>
+  </ol>
+  
+  <ul class="dhonishow">
+    <li><img src="#" /></li>
+    <li><img src="#" /></li>
+    <li><img src="#" /></li>
+  </ul>
+
+  <div class="dhonishow">
+    <div class="dhonishow-element"><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit</p></div>
+    <div class="dhonishow-element"><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit</p></div>
+    <div class="dhonishow-element"><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit</p></div>
+  </div>
+  
+*/
